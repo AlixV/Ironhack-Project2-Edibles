ee0a3afb7fcbef2e3cd9de639da5bd5364aebd99
Armelle Loiseau <armelle.loiseau@gmail.com>
2022-01-17 16:10:29 +0100

    move icons in icons repo

---
+++ routes/recipe.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const RecipeModel = require("./../models/recipe.model");
+const mongoose = require("mongoose");
+const uploader = require("./../config");
