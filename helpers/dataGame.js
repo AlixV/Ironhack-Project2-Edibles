af176b8b9661938765f67055e92c630a0a590c29
Ajr Wum <ajr.wum@gmail.com>
2022-01-18 11:25:49 +0100

    change dataGame.js to put it in backend

---
+++ helpers/dataGame.js
@@ -0,0 +1,19 @@
+const dataGame = {
+  // cardsToPlay
+  BALADE_NB_CARDS: 15,
+  RANDO_NB_CARDS: 30,
+  TREK_NB_CARDS: 45,
+
+  // points at start
+  BALADE_NB_POINTS: 50,
+  RANDO_NB_POINTS: 100,
+  TREK_NB_POINTS: 150,
+
+  // points count during the game
+  EFFECT_PLANT_EDIBLE: 4,
+  EFFECT_PLANT_TOXIC: -4,
+  EFFECT_PLANT_LETHAL: -150,
+};
+
+
+module.exports = { dataGame };
