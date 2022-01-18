507ae10e5b5c162c3b605e3163a52929f45bde86
Alix Virot <flying.cat@mac.com>
2022-01-18 10:19:38 +0100

    Auth and player routes

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-require("dotenv").config();
-require("./config/mongodb"); // database initial setup
-const mongoose = require("mongoose");
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+require('dotenv').config();
+require("./config/mongodb")
+
+// base dependencies
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const flash = require("connect-flash");
 const session = require("express-session");
 const hbs = require("hbs");
-
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-hbs.registerPartials(path.join(__dirname, "views/partials"));
-
-app.use(logger("dev"));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, '/views/partials'))
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// config
+app.use(
+	session({
+		secret:
+			process.env.SESSION_SECRET || "ASecretStringThatSouldBeHARDTOGUESS/CRACK",
+		saveUninitialized: true,
+		resave: true,
+	})
+);
+
+app.use(flash());
+
+// CUSTOM MIDDELWARES
+
+app.use(require("./middlewares/exposeLoginStatus")); // expose le status de connexion aux templates
+app.use(require("./middlewares/exposeFlashMessage")); // affiche les messages dans le template
+
 
 // all routers
 // - index routes
@@ -36,12 +53,13 @@ const recipesRouter = require("./routes/recipes");
 app.use("/recipes", recipesRouter);
 
 // - auth routes
-// const authRouter = require("./routes/auth");
-// app.use("/auth", authRouter);
+const authRouter = require('./routes/auth');
+app.use('/', authRouter);
 
 // - dashboard routes
 // const playerRouter = require('./routes/player');
 // app.use('/player', playerRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -59,15 +77,6 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.use(
-  session({
-    secret:
-      process.env.SESSION_SECRET || "ASecretStringThatSouldBeHARDTOGUESS/CRACK",
-    saveUninitialized: true,
-    resave: true,
-  })
-);
 
-app.use(flash());
 
 module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,124 @@
+// const express = require("express") + const router = new express.Router();
+const router = require("express").Router();
+
+const PlayerModel = require("./../models/Player.model");
+const bcrypt = require("bcrypt"); 
+const protectAuthRoute = require("../middlewares/protectAuthRoute");
+// => Alix aller créer midddelewares DONE
+
+// - Display the SIGNUP FORM  Ensuite add protectAuthRoute 
+router.get("/signup", protectAuthRoute,  (req, res) => {
+    res.render("signup.hbs");
+});
+
+
+// - Display the SIGNIN FORM
+router.get("/signin", protectAuthRoute, (req, res) => {
+    res.render("signin.hbs")
+})
+// => Alix aller créer signin + signup forms dans Views hbs DONE
+
+// // - REGISTER NEW PLAYER
+router.post("/signup", async (req, res, next) => {
+ try {
+     // get the player's input : 
+     const newPlayer = { ...req.body };
+     // check if all mandatory fields are present :
+     if (!newPlayer.pseudo ||
+        !newPlayer.email ||
+        !newPlayer.password ){
+         req.flash("error", "Veuillez renseigner tous les champs"); // => Alix Créer Flash DONE
+         res.redirect("/signup");
+     } else {
+         // check if email exists in db :
+         const foundPlayer = await PlayerModel.findOne({email : newPlayer.email})
+         console.log('foundPlayer', foundPlayer);
+         if(foundPlayer){
+             req.flash("error", "Email déjà enregistré");
+             res.redirect("/signup");
+         }else{
+             // HASH + SALT the password !!
+             const hashedPwd = bcrypt.hashSync(newPlayer.password, 10);
+             newPlayer.password = hashedPwd // pourquoi 2 fois ? Alix
+             console.log(`hashedPwd`, hashedPwd);
+             // Create new player un db :
+             await PlayerModel.create(newPlayer);
+             req.flash( "sucess","Félicitations ! Vous êtes enregistré ! Connectez-vous s'il vous plaît.");
+             res.redirect("/signin");
+         }
+     }
+ } catch (error){
+     // handling errors on form fields
+    // console.log("signup - error: ", error);
+    let errorMessage ="";
+    for(field in error.errors) { //voir "errors" Alix
+        errorMessage += error.errors[field].message +"\n"
+    }
+    req.flash("error", errorMessage); // affiche toutes erreurs
+    res.redirect("/signup");
+ }
+})
+
+// CONNECTION UTILISATEUR DÉJÀ ENREGISTRÉ
+router.post("/signin", async (req, res, next) => {
+    try {
+        // get the player's input :
+        const {email, password} = req.body;
+        console.log('req.body', email, password);
+        // check if all mandatory fields are present :
+        if (!email || !password){ // Pourquoi "||" et non "&&" ?
+            req.flash("error", "Veuillez renseigner tous les champs")
+            res.redirect("/signup");
+        } else {
+          // find the player in db :
+          const foundPlayer = await PlayerModel.findOne({email : email});
+          // console.log('foundPlayer', foundPlayer); 
+          if (!foundPlayer){
+              // console.log('before redirect to signup because email unknown')
+              req.flash("error", "Email inconnu, veuillez vous enregistrer s'il vous plaît");
+              res.redirect("/signup");
+          } else {
+              // compare the passwords via bcrypt :
+              const isPwdOk = bcrypt.compareSync(password, foundPlayer.password);
+              // console.log(`isPwdOk`, isPwdOk);
+              if(!isPwdOk){
+                  // if password problem
+                  req.flash("error", "Informations erronéées");
+                  res.redirect("/signin");
+              } else {
+                   // email & password ok, ready to finish authentication
+                   // transform the foundPlayer into an object :
+                   const foundPlayerObjet = foundPlayer.toObject();
+                   // delete the password for more safety :
+                   delete foundPlayerObjet.password;
+                   // create the currentPlayer in session :
+                   req.session.currentUser = foundPlayerObjet;
+                   // display success message & redirect
+                   req.flash("sucess", "Connection réussie !");
+                   res.redirect("/dashboard") // ROUTE À VÉRIFIER
+                }
+            } 
+        }
+    }catch(error){
+        // handling errors on form fields
+        // console.log("signin - error: ", error);
+        let errorMessage = "";
+        for (field in error.errors) {
+            errorMessage += error.errors[field].message + "\n";
+        }
+        req.flash("error", errorMessage);
+        res.redirect("/signin")
+    }
+})
+
+// LOG OUT
+router.get("/logout", (req, res, next) =>{
+    console.log(req.session.currentUser);
+    req.session.destroy(function (err) {
+    // cannot access session here anymore
+    console.log(req.session.currentUser);
+    res.redirect("/signin");  
+    });
+});
+
+module.exports = router;
